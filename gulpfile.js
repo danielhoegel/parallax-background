cabe8e0cb4445cfb7b8321cba62d58e2b6179915
Daniel Högel <hoegel.daniel@gmail.com>
2018-08-27 03:06:29 +0200

    Move demo index.html outside of docs folder + remove docs folder

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,6 @@ const del = require('del');
 
 const __SRC__ = 'src';
 const __DIST__ = 'dist';
-const __DOCS__ = 'docs';
 
 gulp.task('clear', () =>
    del([__DIST__])
@@ -20,12 +19,8 @@ gulp.task('js', () => gulp.src(__SRC__ + '/*.js')
     .pipe(gulp.dest(__DIST__))
 );
 
-gulp.task('docs', () => gulp.src(__DIST__ + '/*.min.*')
-    .pipe(gulp.dest(__DOCS__))
-);
-
-gulp.task('default', gulp.series([ 'clear', 'js', 'docs' ]));
+gulp.task('default', gulp.series([ 'clear', 'js']));
 
 gulp.task('watch', () => {
-    gulp.watch(__SRC__ + '/*.js', gulp.series(['js', 'docs']));
+    gulp.watch(__SRC__ + '/*.js', gulp.series(['js']));
 });
