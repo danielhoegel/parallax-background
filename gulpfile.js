1fa34e32751a179eed98764bc1e4ff0f13b80d03
Daniel Högel <hoegel.daniel@gmail.com>
2018-08-27 02:59:30 +0200

    Rename demo folder to docs

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const del = require('del');
 
 const __SRC__ = 'src';
 const __DIST__ = 'dist';
-const __DEMO__ = 'demo';
+const __DOCS__ = 'docs';
 
 gulp.task('clear', () =>
    del([__DIST__])
@@ -20,12 +20,12 @@ gulp.task('js', () => gulp.src(__SRC__ + '/*.js')
     .pipe(gulp.dest(__DIST__))
 );
 
-gulp.task('demo', () => gulp.src(__DIST__ + '/*.min.*')
-    .pipe(gulp.dest(__DEMO__))
+gulp.task('docs', () => gulp.src(__DIST__ + '/*.min.*')
+    .pipe(gulp.dest(__DOCS__))
 );
 
-gulp.task('default', gulp.series([ 'clear', 'js', 'demo' ]));
+gulp.task('default', gulp.series([ 'clear', 'js', 'docs' ]));
 
 gulp.task('watch', () => {
-    gulp.watch(__SRC__ + '/*.js', gulp.series(['js', 'demo']));
+    gulp.watch(__SRC__ + '/*.js', gulp.series(['js', 'docs']));
 });
